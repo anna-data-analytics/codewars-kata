5ef0079e601adbf50b270f86368ab0a706725a80
Anna Nguyen <anzitcontact@gmail.com>
2024-12-01 14:56:35 +1300

    test sql

---
+++ Hotel transaction - Booking and Service Payments/booking_and_payment.sql
@@ -0,0 +1,17 @@
+WITH ranked_transactions AS (
+  SELECT
+  transaction,
+  name,
+  receipt||' ('||datetime||')' AS service,
+  RANK() OVER (PARTITION BY transaction ORDER BY datetime) AS rank
+  FROM hotel_transactions
+  )
+SELECT
+transaction,
+name,
+MIN(CASE WHEN rank = 1 THEN service END) AS initial_booking,
+MIN(CASE WHEN rank = 2 THEN service END) AS additional_services
+
+FROM ranked_transactions
+GROUP BY transaction, name
+ORDER BY transaction DESC
